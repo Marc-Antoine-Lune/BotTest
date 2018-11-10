b8a6ca48caedd9790d992ff98a6d2a01cb564684
Marc-Antoine-Lune <luneschima@gmail.com>
2018-11-10 18:33:16 +0100

    test bot

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ import request from 'resquest';
 
 const app = express()
 
-app.set('port', (process.env.PORT || 5000))
+app.set("port", (process.env.PORT || 5000))
 
 app.use(urlencoded({extended: false}))
 app.use(json())
 
 //ROUTES
-app.get('/', function(req, res){
-    res.send('Hi test Chatbot')
+app.get("/", function(req, res){
+    res.send("Hi test Chatbot")
 
 
 })
 
-app.get('/webhook/', function(req, res){
+app.get("/webhook/", function(req, res){
 
-    if(req.query['hub.verify_token']==="MAL"){
-        res.send(res.query['hub.challenge'])
+    if(req.query["hub.verify_token"]==="MAL"){
+        res.send(res.query["hub.challenge"])
     }
     res.send("Mauvais Token")
 
 })
 
-app.listen(app.get('port'), function(){
+app.listen(app.get("port"), function(){
     console.log("running: port")
 
 
