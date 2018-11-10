317a7331798ec357a5a3d2015d831415352ecd8b
Marc-Antoine-Lune <luneschima@gmail.com>
2018-11-10 19:24:39 +0100

    test bot

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 'use strict'
 
-import express from 'express';  
-import { urlencoded, json } from 'bodyParser';
-import request from 'resquest';
+const express = require('express')
+const bodyParser =require('body-Parser')
+const request = require('request')
 
 const app = express()
 
-app.set("port", (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000))
 
-app.use(urlencoded({extended: false}))
-app.use(json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 //ROUTES
-app.get("/", function(req, res){
-    res.send("Hi test Chatbot")
+app.get('/', function(req, res){
+    res.send('Hi test Chatbot')
 
 
 })
 
-app.get("/webhook/", function(req, res){
+app.get('/webhook/', function(req, res){
 
-    if(req.query["hub.verify_token"]==="MAL"){
-        res.send(res.query["hub.challenge"])
+    if(req.query['hub.verify_token']==="MAL"){
+        res.send(res.query['hub.challenge'])
     }
     res.send("Mauvais Token")
 
 })
 
-app.listen(app.get("port"), function(){
+app.listen(app.get('port'), function(){
     console.log("running: port")
 
 
