d98702f6172ba640cc26ac435a1834c9af4701e9
Marc-Antoine-Lune <luneschima@gmail.com>
2018-11-10 17:43:15 +0100

    test Chatbot

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const express =require('express')
+const bodyParser =require('bodyParser')
+const request = require('resquest')
+
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//ROUTES
+app.get('/', function(req, res){
+    res.send('Hi test Chatbot')
+
+
+})
+
+app.get('/webhook/', function(req, res){
+
+    if(req.query['hub.verify_token']==="MAL"){
+        res.send(res.query['hub.challenge'])
+    }
+    res.send("Mauvais Token")
+
+})
+
+app.listen(app.get('port'), function(){
+    console.log("running: port")
+
+
+})
